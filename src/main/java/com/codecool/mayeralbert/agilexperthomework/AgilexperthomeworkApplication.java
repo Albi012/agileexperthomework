66a5ba05624c7f378c3b2becf00660205655183a
Albi012 <mayeralbert12@gmail.com>
2020-11-19 16:43:22 +0100

    Initial commit.

---
+++ src/main/java/com/codecool/mayeralbert/agilexperthomework/AgilexperthomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.mayeralbert.agilexperthomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgilexperthomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgilexperthomeworkApplication.class, args);
+    }
+
+}
